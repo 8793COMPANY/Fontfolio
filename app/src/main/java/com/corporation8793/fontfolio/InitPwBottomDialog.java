666614ada365b70f75f3a72b265a456d97ae6ece
gyeong <gyeongyeongi22@gmail.com>
2021-11-17 11:31:16 +0900

    bottom sheet dialog 작업중

--- app/src/main/java/com/corporation8793/fontfolio/InitPwBottomDialog.java
+++ app/src/main/java/com/corporation8793/fontfolio/InitPwBottomDialog.java
@@ -1,14 +1,20 @@
 package com.corporation8793.fontfolio;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
@@ -28,7 +34,45 @@ public class InitPwBottomDialog extends BottomSheetDialogFragment {
         View view = inflater.inflate(R.layout.init_pw_sheet_layout,container,false);
         TextView sent_email_content = view.findViewById(R.id.sent_email_content);
 
-        sent_email_content.getText().toString().replace("@",email);
+        Log.e("email",email);
+
+        sent_email_content.setText(sent_email_content.getText().toString().replace("@",email+"."));
         return view;
+    }
+
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Log.e("in","!!");
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+
+        dialog.setOnShowListener(dialogInterface -> {
+            BottomSheetDialog bottomSheetDialog = (BottomSheetDialog) dialogInterface;
+            setupRatio(bottomSheetDialog);
+        });
+        return  dialog;
+    }
+
+
+    private void setupRatio(BottomSheetDialog bottomSheetDialog) {
+        //id = com.google.android.material.R.id.design_bottom_sheet for Material Components
+        //id = android.support.design.R.id.design_bottom_sheet for support librares
+        FrameLayout bottomSheet = (FrameLayout)
+                bottomSheetDialog.findViewById(R.id.design_bottom_sheet);
+        BottomSheetBehavior behavior = BottomSheetBehavior.from(bottomSheet);
+        ViewGroup.LayoutParams layoutParams = bottomSheet.getLayoutParams();
+        layoutParams.height = getBottomSheetDialogDefaultHeight();
+        bottomSheet.setLayoutParams(layoutParams);
+        behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+    }
+    private int getBottomSheetDialogDefaultHeight() {
+        return (int) ((getWindowHeight() /1520.0) * 820);
+    }
+    private int getWindowHeight() {
+        // Calculate window height for fullscreen use
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity) getContext()).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics.heightPixels;
     }
 }
