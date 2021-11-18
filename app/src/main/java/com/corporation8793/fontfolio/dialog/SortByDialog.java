3b632f7100db56512cdca876a313e32cc6cd75c0
gyeong <gyeongyeongi22@gmail.com>
2021-11-18 18:35:06 +0900

    sort by dialog push

---
+++ app/src/main/java/com/corporation8793/fontfolio/dialog/SortByDialog.java
@@ -0,0 +1,87 @@
+package com.corporation8793.fontfolio.dialog;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.corporation8793.fontfolio.R;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class SortByDialog extends BottomSheetDialogFragment {
+    LinearLayout close_btn;
+    public SortByDialog(){
+
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.sort_by_sheet_layout,container,false);
+        close_btn = view.findViewById(R.id.close_btn);
+
+        close_btn.setOnClickListener(v->{
+            Log.e("clici","!");
+        });
+
+
+        return view;
+    }
+
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Log.e("in","!!");
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+
+        dialog.setOnShowListener(dialogInterface -> {
+            BottomSheetDialog bottomSheetDialog = (BottomSheetDialog) dialogInterface;
+            setupRatio(bottomSheetDialog);
+        });
+        return  dialog;
+    }
+
+
+    private void setupRatio(BottomSheetDialog bottomSheetDialog) {
+        //id = com.google.android.material.R.id.design_bottom_sheet for Material Components
+        //id = android.support.design.R.id.design_bottom_sheet for support librares
+        FrameLayout bottomSheet = (FrameLayout)
+                bottomSheetDialog.findViewById(R.id.design_bottom_sheet);
+        BottomSheetBehavior behavior = BottomSheetBehavior.from(bottomSheet);
+        ViewGroup.LayoutParams layoutParams = bottomSheet.getLayoutParams();
+        layoutParams.height = getBottomSheetDialogDefaultHeight();
+        bottomSheet.setLayoutParams(layoutParams);
+        behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+    }
+    private int getBottomSheetDialogDefaultHeight() {
+        return (int) ((getWindowHeight() /1520.0) * 820);
+    }
+    private int getWindowHeight() {
+        // Calculate window height for fullscreen use
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity) getContext()).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics.heightPixels;
+    }
+}
--- app/src/main/java/com/corporation8793/fontfolio/fragment/HomeFragment.kt
+++ app/src/main/java/com/corporation8793/fontfolio/fragment/HomeFragment.kt
@@ -5,16 +5,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.corporation8793.fontfolio.MainActivity
 import com.corporation8793.fontfolio.R
+import com.corporation8793.fontfolio.dialog.InitPwBottomDialog
+import com.corporation8793.fontfolio.dialog.SortByDialog
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class HomeFragment : Fragment() {
+class HomeFragment(activity: MainActivity) : Fragment() {
 
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var sort_by_btn: Button
+    var mActivity = activity
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +39,20 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view: View = inflater.inflate(R.layout.fragment_home, container, false)
+        sort_by_btn = view.findViewById(R.id.sort_by_btn)
+
+        val sortByDialog = SortByDialog()
+
+
+
+
+        sort_by_btn.setOnClickListener(View.OnClickListener {
+            sortByDialog.show(
+                mActivity.supportFragmentManager,
+                sortByDialog.tag
+            )
+        })
+
 
         return view
     }
