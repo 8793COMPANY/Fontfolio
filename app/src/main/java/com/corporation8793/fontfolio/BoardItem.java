f5ec23262a36337748f7254b3072cf286e167681
gyeong <gyeongyeongi22@gmail.com>
2021-11-22 11:41:45 +0900

    pull 전 commit

---
+++ app/src/main/java/com/corporation8793/fontfolio/BoardItem.java
@@ -0,0 +1,28 @@
+package com.corporation8793.fontfolio;
+
+public class BoardItem {
+
+    private String board_name;
+    private int board_image;
+
+
+
+    public String getBoard_name() {
+        return board_name;
+    }
+
+    public void setBoard_name(String board_name) {
+        this.board_name = board_name;
+    }
+
+    public int getBoard_image() {
+        return board_image;
+    }
+
+    public void setBoard_image(int board_image) {
+        this.board_image = board_image;
+    }
+
+
+
+}
